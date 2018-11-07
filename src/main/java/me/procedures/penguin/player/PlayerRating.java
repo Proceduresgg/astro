73ce16d7f8a4d3fcef4525b765b76cbc7a3cba11
Proceduresgg <DewgsIsGay143>
2018-11-06 20:05:49 -0600

    Added the ELO formula.

--- src/main/java/me/procedures/penguin/player/PlayerRating.java
+++ src/main/java/me/procedures/penguin/player/PlayerRating.java
@@ -1,5 +1,10 @@
 package me.procedures.penguin.player;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PlayerRating {
 
     private int rating;
--- src/main/java/me/procedures/penguin/utils/GameUtil.java
+++ src/main/java/me/procedures/penguin/utils/GameUtil.java
@@ -1,9 +1,12 @@
 package me.procedures.penguin.utils;
 
+import me.procedures.penguin.player.PlayerRating;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public class GameUtil {
+
+    public static final int kFactor = 32;
 
     public static void resetPlayer(Player player) {
         player.setCanPickupItems(false);
@@ -16,5 +19,22 @@ public class GameUtil {
         player.setHealth(20);
         player.setFoodLevel(20);
         player.setFireTicks(1);
+    }
+
+    public static void updateRating(PlayerRating winner, PlayerRating loser) {
+        double winnerRating = (double) winner.getRating();
+        double loserRating = (double) loser.getRating();
+
+        winnerRating = Math.pow(10, winnerRating / loserRating);
+        loserRating = Math.pow(10, loserRating / winnerRating);
+
+        double winnerExpected = winnerRating / (winnerRating + loserRating);
+        double loserExpected = loserRating / (loserRating + winnerRating);
+
+        winnerRating = winner.getRating() + 32 * (1 - winnerExpected);
+        loserRating = loser.getRating() + 32 * (0 - loserExpected);
+
+        winner.setRating((int) winnerRating);
+        loser.setRating((int) loserRating);
     }
 }
