f86b3626e9a32d885cc0dfd9fda3441c6494c283
Proceduresgg <DewgsIsGay143>
2018-11-25 23:13:27 -0600

    Fixed a small bug.

--- src/main/java/me/procedures/astro/match/options/Unranked.java
+++ src/main/java/me/procedures/astro/match/options/Unranked.java
@@ -6,19 +6,13 @@ import me.procedures.astro.match.team.MatchTeam;
 import me.procedures.astro.utils.CC;
 import me.procedures.astro.utils.StringUtil;
 import mkremins.fanciful.FancyMessage;
-import org.bukkit.entity.Player;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Unranked implements MatchOption {
 
     @Override
     public void startPreGame(Match match) {
-        match.getMatchPlayers().forEach(matchPlayer -> {
-            Player player = matchPlayer.getPlayer();
-
-            player.sendMessage(CC.LIGHT + "Your Opponent: " + CC.BRIGHT + StringUtil.getPlayerNames(match.getPlayers(matchPlayer.getTeam() == MatchTeam.RED ? MatchTeam.BLUE : MatchTeam.RED)));
+        match.getMatchPlayers().forEach((player, matchPlayer) -> {
+            player.sendMessage(CC.LIGHT + "Your Opponent: " + CC.BRIGHT + StringUtil.getPlayerNames(matchPlayer.getTeam() == MatchTeam.RED ? match.getPlayers(MatchTeam.BLUE) : match.getPlayers(MatchTeam.RED)));
         });
     }
 
